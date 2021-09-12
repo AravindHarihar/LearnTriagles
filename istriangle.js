570b6ed6608968d883401444ded4a5eaf5b77642
AravindHarihar <89657178+AravindHarihar@users.noreply.github.com>
2021-09-12 19:39:12 +0530

    isTriangle part html & js

---
+++ istriangle.js
@@ -0,0 +1,25 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleButton = document.querySelector("#is-triangle-btn");
+const output1 = document.querySelector("#output")
+
+
+function isTriangle(){
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value))
+    
+    if(sumOfAngles === 180){
+        output1.innerText = "Yay It is a triangle";
+        console.log("Yay, it is a triangle");
+    } else{
+        output1.innerText = "Uh Oh! It is not a triangle";
+    }
+}
+
+
+function calculateSumOfAngles(angle1, angle2, angle3){
+    const sumOfAngles = angle1 + angle2 + angle3
+    return sumOfAngles;
+}
+
+
+
+isTriangleButton.addEventListener("click", isTriangle);
